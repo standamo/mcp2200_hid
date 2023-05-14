ab7eceb9539781fec21942f401c19193b70408f3
Stan Moravec <stanislav.moravec@hpe.com>
2023-05-14 12:53:58 +0200

    WIP: added args parsing

--- mcp2200_hid.py
+++ mcp2200_hid.py
@@ -1,4 +1,6 @@
 import os
+import fcntl
+import argparse
 
 hidraw_sys = "/sys/class/hidraw"
 hidraw_path = ""
@@ -44,6 +46,38 @@ def mcp2200_set_clear_bits(hidraw_file, set_bitmap, clear_bitmap):
     SET_CLEAR_OUTPUTS[12] = clear_bitmap
     result = write_read(hidraw_file, SET_CLEAR_OUTPUTS)
 
-with open(hidraw_path, "wb+") as hidraw_file:
-    mcp2200_set_clear_bits(hidraw_file, 7, 1)
-    print(f"{mcp2200_read_bmap(hidraw_file):08b}")
+def main():
+    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS, conflict_handler='resolve')
+    parser.add_argument(
+        "--help", "-h", dest="Help", default=False, action="store_true", help="Show help"
+    )
+    parser.add_argument(
+        "--status", "-s", dest="Status", default=None, action="store", help="Status of given GPIO port"
+    )   
+    parser.add_argument(
+        "--enable", "-e", dest="Enable", default=None, action="store", help="Enable (set) GPIO port"
+    )   
+    parser.add_argument(
+        "--disable", "-d", dest="Disable", default=None, action="store", help="Disable (clear) GPIO port"
+    )   
+    args = parser.parse_args()
+    
+    if (args.Help):
+        parser.print_help()
+        exit(0)    
+
+    with open(hidraw_path, "wb+") as hidraw_file:
+        # Acquire a lock on the HID device file
+        fcntl.flock(hidraw_file.fileno(), fcntl.LOCK_EX)
+        try:
+            # Perform read/write operations on the HID device
+            mcp2200_set_clear_bits(hidraw_file, 7, 1)
+            if args.Status:
+                print(f"{mcp2200_read_bmap(hidraw_file):08b}")
+
+        finally:
+            # Release the lock on the HID device file
+            fcntl.flock(hidraw_file.fileno(), fcntl.LOCK_UN)    
+
+if __name__ == "__main__":
+    main()
